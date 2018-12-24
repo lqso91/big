32b845704dc0dbd3ae72e5bd9e17ec7916cf2cdc
de-0o0 <lqso91@gmail.com>
2018-12-24 17:25:02 +0800

    编写mr-word count

---
+++ mapreduce/src/main/java/top/lqso/mr/WordCountDriver.java
@@ -0,0 +1,41 @@
+package top.lqso.mr;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCountDriver extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(getConf(), "word-count");
+        job.setJarByClass(getClass());
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+//        job.setCombinerClass(WordCountReducer.class);
+
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int run = ToolRunner.run(new Configuration(), new WordCountDriver(), args);
+        System.exit(run);
+    }
+}
---
+++ mapreduce/src/main/java/top/lqso/mr/WordCountMapper.java
@@ -0,0 +1,21 @@
+package top.lqso.mr;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+    private Text word = new Text();
+    private LongWritable count = new LongWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] split = value.toString().split(" ");
+        for (String s: split) {
+            word.set(s);
+            context.write(word, count);
+        }
+    }
+}
