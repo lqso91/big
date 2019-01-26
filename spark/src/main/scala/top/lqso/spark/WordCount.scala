591fc6dadd36fffff365b3c63107c200826bbe40
de-0o0 <lqso91@gmail.com>
2019-01-26 15:02:17 +0800

    spark练习

---
+++ spark/src/main/scala/top/lqso/spark/WordCount.scala
@@ -0,0 +1,22 @@
+  package top.lqso.spark
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("wordCount")
+      .setMaster("local")
+
+    val sc = new SparkContext(conf)
+    sc.textFile("E:\\1\\words.txt")
+      .flatMap(_.split(" "))
+      .map((_,1))
+      .reduceByKey(_+_)
+      .sortBy(_._2, false)
+      .collect()
+      .foreach(println)
+
+    sc.stop();
+  }
+}
